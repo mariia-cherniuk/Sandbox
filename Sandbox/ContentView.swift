391a62601171a59cde0f3e14eea040c07af4db30
Mariia Cherniuk <marydort1@gmail.com>
2020-07-13 15:06:52 +0100

    LazyHGrid

--- Sandbox/ContentView.swift
+++ Sandbox/ContentView.swift
@@ -10,32 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
-    @Environment(\.horizontalSizeClass) var sizeClass
-    
     var layout = [
-        GridItem(.fixed(50)),
-        GridItem(.fixed(50))
+        GridItem(.adaptive(minimum: 100))
     ]
     
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: layout, alignment: .leading, spacing: 100, pinnedViews: .sectionHeaders) {
-                Section(header:
-                            Text("Header")
-                            .font(.title)
-                            .padding()
-                ) {
-                    ForEach(1..<100) { i in
-                        RoundedRectangle(cornerRadius: 20, style: .continuous)
-                            .fill(Color.random())
-                            .frame(height: 200)
+            ForEach(0..<5) { i in
+                ScrollView(.horizontal) {
+                    LazyHGrid(rows: layout) {
+                        ForEach(0..<100) { i in
+                            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                .fill(Color.random())
+                                .frame(width: 100, height: 100)
+                        }
                     }
                 }
             }
         }
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
