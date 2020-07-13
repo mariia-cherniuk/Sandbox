e82d9ed2c647e314f02e2ec29ddd9976d228a908
Mariia Cherniuk <marydort1@gmail.com>
2020-07-13 09:56:20 +0100

    Initial commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Paul Hudson on 10/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
