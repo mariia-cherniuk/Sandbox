82e4144b362db3236c84c81362e72455a255b9d9
Mariia Cherniuk <marydort1@gmail.com>
2020-07-14 14:04:36 +0100

    ScaledMetric + TabBar

--- Sandbox/ContentView.swift
+++ Sandbox/ContentView.swift
@@ -5,66 +5,34 @@
 //  Created by Paul Hudson on 10/07/2020.
 //
 
-import MapKit
 import SwiftUI
-
-struct PrimaryView: View {
-    
-    @State private var selection: Int?
-    
-    var body: some View {
-        List(0..<5, selection: $selection) { i in
-            NavigationLink(destination:
-                            SupplementaryView(id: i)) {
-                Text("Item \(i)")
-            }
-        }
-        .navigationTitle("Menu")
-        .listStyle(SidebarListStyle())
-    }
-}
-
-struct SupplementaryView: View {
-    
-    @State private var selection: Int?
-    
-    let id: Int
-    
-    var body: some View {
-        List(0..<5, selection: $selection) { i in
-            NavigationLink(destination:
-                            SecondaryView(group: i, id: id)) {
-                Text("Row \(i)")
-            }
-        }
-        .navigationTitle("Menu")
-        .listStyle(InsetGroupedListStyle())
-    }
-}
-
-struct SecondaryView: View {
-    
-    let group: Int
-    let id:  Int
-    
-    var body: some View {
-        Text("Destination \(id) - \(group)")
-    }
-}
 
 struct ContentView: View {
     
+    @ScaledMetric var frame: CGFloat = 100
+    
     var body: some View {
-        NavigationView {
-            PrimaryView()
-            SupplementaryView(id: 0)
-            SecondaryView(group: 0, id: 0)
+        TabView {
+            VStack {
+                Text("Hello")
+                    .font(.largeTitle)
+                
+                Image("Spain")
+                    .resizable()
+                    .aspectRatio(2, contentMode: .fit)
+                    .frame(width: frame)
+            }
+            .tabItem {
+                Image(systemName: "star")
+                    .resizable()
+                Text("Star")
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environment(\.sizeCategory, .accessibilityExtraExtraLarge)
     }
 }
